14edcacc88bd01defdf315d1177608e356a10b4c
nownabe <nownabe@gmail.com>
2016-08-02 21:57:10 +0900

    :books: Fix amazon filter

--- lib/filters/amazon_associate.rb
+++ lib/filters/amazon_associate.rb
@@ -22,7 +22,7 @@ module Filters
 
     class Asin
       MAX_RETRY = 5
-      RETRY_INTERVAL = 5
+      RETRY_INTERVAL = 10
 
       attr_reader :id
 
@@ -77,12 +77,16 @@ module Filters
         @item ||= lookup.items.first
       end
 
-      def lookup(retry_count = 0)
+      def lookup(retry_count: MAX_RETRY)
         Amazon::Ecs.item_lookup(id)
-      rescue => e
-        if retry_count < MAX_RETRY
+      rescue Amazon::RequestError => e
+        $stderr.puts "failed to lookup (retry_count #{retry_count})"
+        $stderr.puts e.class
+        $stderr.puts e.message
+        if retry_count > 0
+          retry_count -= 1
           sleep(RETRY_INTERVAL)
-          lookup(retry_count + 1)
+          retry
         end
         raise e
       end
